4943d0c8b7f58e561a4619f7a9cd77d5adb6a811
Сергей Винчестер <creepmah@gmail.com>
2023-10-05 09:41:20 +0300

    Добавьте файлы проекта.

---
+++ ImagesWebParser/ImageParser.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace ImagesWebParser
+{
+    public static class ImageParser
+    {
+        public static async Task DownloadImageByURL(string url, string outputDirectory)
+        {
+            if (!Directory.Exists(outputDirectory))
+            {
+                Directory.CreateDirectory(outputDirectory);
+            }
+
+            using (WebClient client = new WebClient())
+            {
+                string fileName = url.Split('/').LastOrDefault() ?? $"file_{DateTime.Now:d}_{DateTime.Now:mmss}";
+                fileName = $"{outputDirectory}/{fileName}.webp";
+
+                await client.DownloadFileTaskAsync(new Uri(url), fileName);
+            }
+        }
+    }
+}
---
+++ ImagesWebParser/Program.cs
@@ -0,0 +1,13 @@
+﻿using ImagesWebParser;
+
+const string URL_PART = "https://www.carmanualsonline.info/img/37/22332/w960_22332-";
+const string OUTPUT_DIRECTORY_WEBP = "Downloaded Images";
+const int LAST_PAGE = 406;
+
+for (int i = 1; i <= LAST_PAGE; i++)
+{
+    string url = $"{URL_PART}{i}.png";
+    await ImageParser.DownloadImageByURL(url, OUTPUT_DIRECTORY_WEBP);
+    Console.WriteLine(LAST_PAGE - i);
+    await Task.Delay(5);
+}
